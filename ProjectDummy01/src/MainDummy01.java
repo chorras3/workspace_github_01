ae936c3585bc2fe446c21c1d784cef7e06359594
expf0043 <expf0043@SGPF-EXT-JGL.oficinas.sepg.minhac.age>
2018-05-21 12:28:53 +0200

    Project "ProjectDummy01" first upload

---
+++ ProjectDummy01/src/MainDummy01.java
@@ -0,0 +1,13 @@
+public class MainDummy01 {
+
+	public static void main(String[] args){
+		MainDummy01 application = new MainDummy01();
+		application.test01();
+		System.exit(0);
+	}
+	
+	public void test01(){
+		System.out.println("test01()");
+	}
+
+}

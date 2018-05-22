46506671593e02f7c5f2e9da20b784322c39f70f
expf0043 <expf0043@SGPF-EXT-JGL.oficinas.sepg.minhac.age>
2018-05-22 14:39:27 +0200

    Rutinary changes

---
+++ 2018-05-22_0727_JActions/src/useExamples/MainTest01.java
@@ -0,0 +1,25 @@
+package useExamples;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
+public class MainTest01 {
+	public static void main(String[] args){
+		MainTest01 application = new MainTest01();
+		application.runQuick();
+		System.exit(0);
+	}
+	
+	
+	protected void runQuick(){
+		System.out.println("runQuick()");
+		
+		
+				
+	}
+		
+	
+	
+}
